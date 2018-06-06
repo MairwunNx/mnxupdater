fa94ecdef364b66dcee11e2226769a6e64799355
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-06-06 22:37:51 +0300

    Added sources for Updater.

---
+++ MairwunNxUpdater/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MairwunNxUpdater
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("<!== MairwunNx Updater Tool : 1.0R Rv1 B1 (060618) ==!>\n");
+
+            try
+            {
+                string process = args[1].Replace(".exe", "");
+
+                Console.WriteLine("<!== Starting update! Close updatable applications ==!>\n");
+
+                while (Process.GetProcessesByName(process).Length > 0)
+                {
+                    Process[] myProcesses2 = Process.GetProcessesByName(process);
+
+                    for (int i = 1; i < myProcesses2.Length; i++) { myProcesses2[i].Kill(); }
+                }
+
+                if (File.Exists(args[1])) { File.Delete(args[1]); }
+
+                File.Move(args[0], args[1]);
+
+                Console.WriteLine("<!== Update done! Your application will now start! ==!>\n");
+
+                Thread.Sleep(200);
+
+                Process.Start(args[1]); Process.GetCurrentProcess().Kill();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("<!== Update finished with errors, sorry please :(( ==!>\n");
+
+                Console.WriteLine(exception.ToString());
+            }
+        }
+    }
+}
---
+++ MairwunNxUpdater/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("MairwunNxUpdater")]
+[assembly: AssemblyDescription("Simple updater for projects by MairwunNx.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MairwunNxUpdater")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 MairwunNx.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3c0958e3-e64b-4695-a493-0ba2d8bcfc56")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
